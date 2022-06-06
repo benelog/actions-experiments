bacecaf64f23cf755a03a680c52c58e7cd3a2014
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2022-06-06 11:40:19 +0900

    크리스마스에 대한 테스트 추가

--- src/test/java/util/DateTimesTest.java
+++ src/test/java/util/DateTimesTest.java
@@ -15,4 +15,11 @@ class DateTimesTest {
 		boolean holiday = DateTimes.isHoliday(day);
 		assertThat(holiday).isTrue();
 	}
+
+	@Test
+	void christmasIsHoliday() {
+		LocalDate day = LocalDate.of(2020, 12, 25);
+		boolean holiday = DateTimes.isHoliday(day);
+		assertThat(holiday).isTrue();
+	}
 }
