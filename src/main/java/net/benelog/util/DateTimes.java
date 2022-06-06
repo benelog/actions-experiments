0ed787bb57c5aa12b3d78539ce4927fceac6c75c
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2022-06-06 12:22:21 +0900

    크리스마스를 공휴일로

--- src/main/java/net/benelog/util/DateTimes.java
+++ src/main/java/net/benelog/util/DateTimes.java
@@ -2,11 +2,23 @@ package net.benelog.util;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.MonthDay;
+import java.util.Set;
 
 public class DateTimes {
+	private static final Set<MonthDay> LEGAL_HOLIDAYS = Set.of(
+		MonthDay.of(Month.DECEMBER, 25)
+	);
+
 	public static boolean isHoliday(LocalDate day) {
 		DayOfWeek dayOfWeek = day.getDayOfWeek();
 		if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+			return true;
+		}
+
+		MonthDay monthDay = MonthDay.of(day.getMonth(), day.getDayOfMonth());
+		if (LEGAL_HOLIDAYS.contains(monthDay)) {
 			return true;
 		}
 		return false;
