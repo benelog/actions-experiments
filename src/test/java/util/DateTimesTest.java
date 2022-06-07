866ca78def4fb9011ba129fbb056ab3b47295bb4
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2022-06-07 14:55:41 +0900

    테스트 리팩토링

--- src/test/java/util/DateTimesTest.java
+++ src/test/java/util/DateTimesTest.java
@@ -3,30 +3,43 @@ package util;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import net.benelog.util.DateTimes;
 
 class DateTimesTest {
-	@Test
-	void sundayIsHoliday() {
-		LocalDate day = LocalDate.of(2022, 6, 5);
-		boolean holiday = DateTimes.isHoliday(day);
-		assertThat(holiday).isTrue();
+	@ParameterizedTest
+	@MethodSource("provideHolidays")
+	void isHoliday(LocalDate day) {
+		boolean actual = DateTimes.isHoliday(day);
+		assertThat(actual).isTrue();
 	}
 
-	@Test
-	void christmasIsHoliday() {
-		LocalDate day = LocalDate.of(2020, 12, 25);
-		boolean holiday = DateTimes.isHoliday(day);
-		assertThat(holiday).isTrue();
+	@ParameterizedTest
+	@MethodSource("provideWorkingDays")
+	void isWorkingDay(LocalDate day) {
+		boolean actual = DateTimes.isHoliday(day);
+		assertThat(actual).isFalse();
 	}
 
-	@Test
-	void childrenDayIsHoliday() {
-		LocalDate day = LocalDate.of(2020, 5, 5);
-		boolean holiday = DateTimes.isHoliday(day);
-		assertThat(holiday).isTrue();
+	static Stream<LocalDate> provideHolidays() {
+		return Stream.of(
+			LocalDate.of(2022, 6, 4), // 토요일
+			LocalDate.of(2022, 6, 5), // 일요일
+			LocalDate.of(2022, 8, 15), // 광복절
+			LocalDate.of(2020, 12, 25), // 크리스마스
+			LocalDate.of(2020, 5, 5) // 어린이날
+		);
+	}
+
+	static Stream<LocalDate> provideWorkingDays() {
+		return Stream.of(
+			LocalDate.of(2020, 7, 17),
+			LocalDate.of(2022, 4, 5),
+			LocalDate.of(2022, 6, 7)
+		);
 	}
 }
